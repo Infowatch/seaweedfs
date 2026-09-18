25b2ddb3aba5d87bdc2c5fdc29fed72fd0d67779
rockbeen <62306907+rockbeen@users.noreply.github.com>
2026-09-18 13:24:31 +0300

    Update volume_loading.go

--- weed/storage/volume_loading.go
+++ weed/storage/volume_loading.go
@@ -64,12 +64,12 @@ func (v *Volume) load(alsoLoadIndex bool, createDatIfMissing bool, needleMapKind
 			dataFile, err = os.Open(v.FileName(".dat"))
 			v.noWriteOrDelete = true
 		}
+		if err != nil {
+			return fmt.Errorf("cannot open volume data file %s: %w", v.FileName(".dat"), err)
+		}
 		v.lastModifiedTsSeconds = uint64(modifiedTime.Unix())
 		if fileSize >= super_block.SuperBlockSize {
 			alreadyHasSuperBlock = true
-		}
-		if err != nil {
-			return fmt.Errorf("cannot open volume data file %s: %w", v.FileName(".dat"), err)
 		}
 		v.DataBackend = backend.NewDiskFile(dataFile)
 	} else {
